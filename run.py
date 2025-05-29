63ebcbe8a5179d072c3755a56b38408118446103
Dave-MK <davidmkilgallon@gmail.com>
2025-05-29 02:54:10 +0100

    Adjust function to validate all values are integers

--- run.py
+++ run.py
@@ -30,12 +30,16 @@ def validate_data(values):
     Validates the input data to ensure it is a list of six integers.
     """
     try:
+        [int(values) for values in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly six values required, you provided {len(values)}"
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
-
+    
+    
+    
+    
 get_sales_data()
 
